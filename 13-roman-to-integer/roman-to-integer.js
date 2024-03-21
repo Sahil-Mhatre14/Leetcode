9ccbd6b3d57bc51c08ebbe4e7764fe82def587b5
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2024-03-21 23:45:05 +0530

    Time: 123 ms (31.69%) | Memory: 55.4 MB (22.20%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+ /*
+    1. Maintain hashMap of char & values
+    2. For each char in s,
+        if hashMap(nextChar) <= hashMap(char),
+         res = res + hashMap(char)
+        else
+         res  res + hashMap(char)
+    3. return res
+         
+ */
+var romanToInt = function(s) {
+    const hashMap = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    var res = 0;
+
+    for (var i = 0; i < s.length; i++) {
+        if (!hashMap[s?.[i+1]]) {
+            res = res + hashMap[s?.[i]]
+        } else if (hashMap[s?.[i+1]] <= hashMap[s?.[i]]) {
+            res = res + hashMap[s?.[i]]
+        } else {
+            res = res - hashMap[s?.[i]]
+        }
+    }
+    return res
+};
