229499559b9cd37517b9dc72f5780e7a6ed82839
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2024-07-06 23:09:38 +0530

    Time: 37 ms (99.89%), Space: 51.2 MB (25.42%) - LeetHub

--- 0202-happy-number/0202-happy-number.js
+++ 0202-happy-number/0202-happy-number.js
@@ -6,54 +6,54 @@
  /*
 
  */
-// var isHappy = function(n) {
-//     var sum = 0
-//     var hashMap = {}
-
-//     var sumOfSquares = (number) => {
-//         var sum = 0
-//         while(number>0) {
-//             var temp = number % 10
-//             sum = sum + Math.pow(temp, 2)
-//             number = Math.floor(number / 10)
-//         }
-//         return sum
-//     }
-
-//     while (!(n in hashMap)) {
-//         hashMap[n] = n
-//         var sum = sumOfSquares(n)
-//         if (sum === 1) {
-//             return true
-//         }
-//         n = sum
-//     } 
-//     return false
-   
-// };
-
 var isHappy = function(n) {
+    var sum = 0
     var hashMap = {}
 
-    var findSum = (num) => {
-        var res = 0;
-        while (num > 0) {
-            var unit = num % 10;
-            res = res + Math.pow(unit, 2)
-            num = Math.floor(num / 10)
+    var sumOfSquares = (number) => {
+        var sum = 0
+        while(number>0) {
+            var temp = number % 10
+            sum = sum + Math.pow(temp, 2)
+            number = Math.floor(number / 10)
         }
-        n = res
-        return res
+        return sum
     }
 
-    while (true) {
-        var sum = findSum(n)
+    while (!(n in hashMap)) {
+        hashMap[n] = n
+        var sum = sumOfSquares(n)
         if (sum === 1) {
             return true
-        } else if (hashMap[sum]) {
-            return false
-        } else {
-            hashMap[sum] = sum
         }
-    }
-}
+        n = sum
+    } 
+    return false
+   
+};
+
+// var isHappy = function(n) {
+//     var hashMap = {}
+
+//     var findSum = (num) => {
+//         var res = 0;
+//         while (num > 0) {
+//             var unit = num % 10;
+//             res = res + Math.pow(unit, 2)
+//             num = Math.floor(num / 10)
+//         }
+//         n = res
+//         return res
+//     }
+
+//     while (true) {
+//         var sum = findSum(n)
+//         if (sum === 1) {
+//             return true
+//         } else if (hashMap[sum]) {
+//             return false
+//         } else {
+//             hashMap[sum] = sum
+//         }
+//     }
+// }
