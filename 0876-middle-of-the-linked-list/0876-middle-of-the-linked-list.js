52184e509e9a69d20a3fde8a8c4b114d92e3560b
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2024-09-27 20:01:33 -0700

    Time: 57 ms (23.29%), Space: 48.4 MB (88.64%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -18,27 +18,48 @@
     return node at index n/2
  }
  */
+// var middleNode = function(head) {
+//     var n = 0
+//     var ptr = head
+//     var targetNodeIndex = 0
+
+//     while (ptr) {
+//         n++
+//         ptr = ptr.next
+//     }
+//     if (n % 2 != 0) {
+//         targetNodeIndex = Math.floor(n/2) 
+//     } else {
+//         targetNodeIndex = n/2
+//     }
+
+//     var c = 0
+//     ptr = head
+
+//     while (c != targetNodeIndex) {
+//         c++
+//         ptr = ptr.next
+//     }
+//     return ptr
+// };
+
+/*
+1.initialize slow and fast pointers to head
+2. while(fast && fast.next) {
+    slow = snow.next
+    fast = fast.next.next
+}
+3.return slow
+*/
+
 var middleNode = function(head) {
-    var n = 0
-    var ptr = head
-    var targetNodeIndex = 0
+    var slow = head
+    var fast = head
 
-    while (ptr) {
-        n++
-        ptr = ptr.next
-    }
-    if (n % 2 != 0) {
-        targetNodeIndex = Math.floor(n/2) 
-    } else {
-        targetNodeIndex = n/2
+    while(fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
     }
 
-    var c = 0
-    ptr = head
-
-    while (c != targetNodeIndex) {
-        c++
-        ptr = ptr.next
-    }
-    return ptr
-};
+    return slow
+}
