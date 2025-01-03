e39de9f8aa6a31f32d4c4c8bbd8a5b2b24f69697
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2025-01-03 08:09:26 -0800

    Time: 9 ms, Memory: 49.6 MB - LeetHub

---
+++ 10985-242-647-palindromic-substrings/10985-242-647-palindromic-substrings.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+ /*
+ Same as longest palindromic substring
+ maintain a couter res
+ increment it for each palindromic substring found
+ */
+var countSubstrings = function(s) {
+    let res = 0
+
+    for (let i = 0; i < s.length; i++) {
+        let l = i
+        let r = i
+
+        while (l >=0 && r < s.length && s[l] == s[r]) {
+            res++
+            l--
+            r++
+        }
+
+        l = i
+        r = i+1
+
+        while (l >=0 && r < s.length && s[l] == s[r]) {
+            res++
+            l--
+            r++
+        }
+
+    }
+    return res
+};
