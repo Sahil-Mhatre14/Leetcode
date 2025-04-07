e71af2fe44fd37e5823ed42ef6ffe43dc3398b5c
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2025-04-07 08:51:36 -0700

    Time: 0 ms, Memory: 53.3 MB - LeetHub

---
+++ 22039-466-198-house-robber/22039-466-198-house-robber.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+/*
+1. keep track of sum of money robbed from prev 2 houses
+2. iterate over houses
+3. rob from house[i] if sum of money robbed from house [i] & prev to prev house is more than
+sum of money robbed from prev house
+*/
+
+var rob = function(nums) {
+    var rob2 = 0
+    var rob1= 0 
+
+    for (var i = 0; i < nums.length; i++) {
+        var temp = Math.max(nums[i] + rob1, rob2)
+        rob1 = rob2
+        rob2 = temp
+    }
+    return rob2
+};
