eead7c0976f876adf827eeec46b74597b612f950
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2025-09-01 17:29:40 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,29 @@
+'''
+1. Use binary search from min eating speed to max eating speed
+min eating speed (l) = min(piles)
+max eating speed (r) = max(piles)
+'''
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        piles.sort()
+        l = piles[0]
+        r = piles[-1]
+        res = r
+
+        while (l <= r):
+            mid = (l + r) // 2
+            hours = 0
+
+            for pile in piles:
+                hours = hours + math.ceil(pile / mid)
+
+            if hours <= h:
+                res = min(res, mid)
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return res
+
+        
