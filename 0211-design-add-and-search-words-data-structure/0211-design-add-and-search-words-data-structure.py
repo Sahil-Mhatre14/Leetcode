79f6850a072eb5453648397b41993e777d476551
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2025-09-19 08:55:17 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,4 +1,4 @@
-class TrieNode:
+class TrieNode():
     def __init__(self):
         self.children = {}
         self.endOfWord = False
@@ -12,7 +12,7 @@ class WordDictionary:
         curr = self.root
 
         for ch in word:
-            if ch not in curr.children:
+            if not(ch in curr.children):
                 curr.children[ch] = TrieNode()
             curr = curr.children[ch]
         
@@ -20,24 +20,25 @@ class WordDictionary:
 
     def search(self, word: str) -> bool:
         
-        def dfs(root, j):
+        def dfs(root, i):
             curr = root
 
-            for i in range(j, len(word)):
-                ch = word[i]
+            for j in range(i, len(word)):
+                ch = word[j]
 
-                if ch != '.':
-                    if ch not in curr.children:
-                        return False
-                    curr = curr.children[ch]
-                else:
-                    for children in curr.children.values():
-                        if (dfs(children, i + 1)):
+                if (ch == '.'):
+                    for child in curr.children:
+                        if dfs(child, j + 1):
                             return True
                     return False
-            return curr.endOfWord   
 
-        return dfs(self.root, 0)    
+                else:
+                    if not(ch in curr.children):
+                        return False
+                    curr = curr.children[ch]
+            return curr.endOfWord
+
+        dfs(self.root, 0)
 
 
 # Your WordDictionary object will be instantiated and called as such:
