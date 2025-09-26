31b70797ce3806fc7a4e049900b2fcf20b8fac63
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2025-09-26 10:36:18 -0700

    Time: 35 ms (52.06%), Space: 19.6 MB (28.9%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -6,26 +6,26 @@ class Solution:
         rows = len(heights)
         cols = len(heights[0])
 
-        def bfs(r,c,visited,prevHeight):
+        def dfs(r,c,visited,prevHeight):
             if (r < 0 or r >= rows or c < 0 or c >= cols
             or (r,c) in visited or heights[r][c] < prevHeight ):
                 return
 
             visited.add((r,c))
 
-            bfs(r, c+1, visited, heights[r][c])
-            bfs(r, c-1, visited, heights[r][c])
-            bfs(r+1, c, visited, heights[r][c])
-            bfs(r-1, c, visited, heights[r][c])
+            dfs(r, c+1, visited, heights[r][c])
+            dfs(r, c-1, visited, heights[r][c])
+            dfs(r+1, c, visited, heights[r][c])
+            dfs(r-1, c, visited, heights[r][c])
 
 
         for c in range(cols):
-            bfs(0, c, pac, heights[0][c])
-            bfs(rows - 1, c, atl, heights[rows-1][c])
+            dfs(0, c, pac, heights[0][c])
+            dfs(rows - 1, c, atl, heights[rows-1][c])
 
         for r in range(rows):
-            bfs(r, 0, pac, heights[r][0])
-            bfs(r, cols - 1, atl, heights[r][cols-1])
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, cols - 1, atl, heights[r][cols-1])
 
         for r in range(rows):
             for c in range(cols):
