3a76dc77a635193ca29137ede2d975b2426c55e5
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2025-10-25 10:59:16 -0700

    Time: 0 ms (100%), Space: 17.9 MB (17.46%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,5 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+            
         def helper(homes):
             rob1 = 0
             rob2 = 0
@@ -10,8 +13,5 @@ class Solution:
                 rob2 = temp
 
             return rob2
-        
-        if len(nums) == 1:
-            return nums[0]
 
         return max(helper(nums[:-1]), helper(nums[1:]))
