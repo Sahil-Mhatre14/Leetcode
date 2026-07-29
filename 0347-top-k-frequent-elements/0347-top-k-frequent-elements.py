a3fde406a4fb706ebc49f29b92f76f73472615a9
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2026-07-28 22:12:51 -0700

    Time: 11 ms (25.17%), Space: 24.9 MB (12.51%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,4 +1,4 @@
-'''
+"""
 1. create a hashMap to count frequency of elements in nums
 res = []
 2. bucket = create a new array and initialize it with len(nums) + 1 empty arrays
@@ -9,23 +9,24 @@ res = []
             bucket[value] = [key]
 4. Loop in reverse order over bucket:
         put elements from bucket while k > 0
-'''
+"""
+
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashMap ={}
+        hashMap = {}
         res = []
 
         for n in nums:
             hashMap[n] = hashMap.get(n, 0) + 1
-        
+
         bucket = []
         for i in range(len(nums) + 1):
             bucket.append([])
 
         for key, val in hashMap.items():
             bucket[val].append(key)
-        
+
         for i in range(len(bucket) - 1, -1, -1):
             if len(bucket[i]) > 0:
                 for n in bucket[i]:
@@ -33,7 +34,5 @@ class Solution:
                     k -= 1
                     if k == 0:
                         return res
-        
+
         return res
-                
-                
