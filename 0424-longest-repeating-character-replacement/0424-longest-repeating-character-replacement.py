b41693c67748e0b1f269a45d17e9188cfb44e57e
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2026-08-18 22:50:51 -0700

    Time: 158 ms (18.55%), Space: 19.7 MB (15.61%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -2,8 +2,8 @@
 1. l = 0, r = 0, res = 0
 charMap = {}
 2.
-for i from 0 to end:
-    charMap[s[i]] ++
+for r from 0 to end:
+    charMap[s[r]] ++
     while (r - l + 1 ) - max frequence > k:
         decerement frequency of s[l]
         l += 1
@@ -14,18 +14,19 @@ for i from 0 to end:
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
         l = 0
         r = 0
-        res = 0
         charMap = {}
 
-        for r in range(0, len(s)):
+        while r < len(s):
             charMap[s[r]] = charMap.get(s[r], 0) + 1
-    
-            while ((r - l + 1) - max(charMap.values()) > k):
+            
+            while((r - l + 1) - max(charMap.values()) > k):
                 charMap[s[l]] -= 1
                 l += 1
-            
+
             res = max(res, r - l + 1)
+            r += 1
         
         return res
