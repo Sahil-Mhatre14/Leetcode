9180b5689863b85629bf9b584f3b905239587288
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2026-08-19 22:44:33 -0700

    Time: 19 ms (60.09%), Space: 19.4 MB (18.46%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -5,26 +5,23 @@ class Solution:
 
         if len(s1) > len(s2):
             return False
-
+        
         for i in range(len(s1)):
             map1[s1[i]] = map1.get(s1[i], 0) + 1
             map2[s2[i]] = map2.get(s2[i], 0) + 1
 
         def areMapsEqual():
-            # for char in map1.keys():
-            #     if map1[char] != map2.get(char, 0):
-            #         return False
-            # return True
             return map1 == map2
-
+        
         for i in range(0, len(s2) - len(s1)):
             if areMapsEqual():
                 return True
             
-            map2[s2[i]] = map2[s2[i]] - 1
+            map2[s2[i]] -= 1
             if map2[s2[i]] == 0:
                 del map2[s2[i]]
-                
+            
             map2[s2[i + len(s1)]] = map2.get(s2[i + len(s1)], 0) + 1
-
+            i = i + len(s1)
+        
         return True if areMapsEqual() else False
