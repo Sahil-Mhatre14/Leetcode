b7e04f2d9b9618d5e838c3665b46514bfd335e38
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2026-09-16 22:28:09 -0700

    Time: 4152 ms (39.58%), Space: 19.6 MB (28.99%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,27 +1,25 @@
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        visited = set()
+    def exist(self, board: list[list[str]], word: str) -> bool:
         row = len(board)
         col = len(board[0])
+        visited = set()
 
-        def dfs(r, c, i):
-            if i == len(word):
+        def dfs(r,c,k):
+            if k >= len(word):
                 return True
 
-            if (r < 0 or c < 0 or r >= row or c >= col 
-            or (r,c) in visited or board[r][c] != word[i]):
+            if r >= row or r < 0 or c >= col or c < 0 or board[r][c] != word[k] or (r,c) in visited:
                 return False
 
             visited.add((r,c))
-            res = dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1)
+            
+            res = dfs(r+1, c, k+1) or dfs(r-1,c,k+1) or dfs(r, c+1, k+1) or dfs(r, c-1, k+1)
             visited.remove((r,c))
-
             return res
 
-
-        for r in range(row):
-            for c in range(col):
-                if (dfs(r, c, 0)):
+        for i in range(row):
+            for j in range(col):
+                if dfs(i,j,0):
                     return True
         
         return False
