8ab569a2254ef4e908ee559769bf8f16ba001283
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2026-09-17 22:40:27 -0700

    Time: 0 ms (100%), Space: 19.4 MB (10.21%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,39 +1,6 @@
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        # res= []
-        
-        # digitsMap = {
-        #     "2": "abc",
-        #     "3": "def",
-        #     "4": "ghi",
-        #     "5": "jkl",
-        #     "6": "mno",
-        #     "7": "qprs",
-        #     "8": "tuv",
-        #     "9": "wxyz",
-        # }
-
-        
-
-        # def backtrack(i, currStr):
-        #     if i >= len(digits):
-        #         res.append(currStr)
-        #         return
-            
-        #     chars = digitsMap[digits[i]]
-
-        #     for char in chars:
-        #         backtrack(i + 1, currStr + char)
-            
-        # if len(digits) > 0:
-        #     backtrack(0, "")
-            
-        # return res
-
-        res = []
-        currStr = []
-
-        charMap = {
+    def letterCombinations(self, digits: str) -> list[str]:
+        digitsMap = {
             "2": "abc",
             "3": "def",
             "4": "ghi",
@@ -44,18 +11,18 @@ class Solution:
             "9": "wxyz"
         }
 
-        def dfs(i, currStr):
+        res = []
+        currStr = []
+
+        def backtrack(i):
             if i >= len(digits):
                 res.append("".join(currStr))
                 return
 
-            for ch in charMap[digits[i]]:
+            for ch in digitsMap[digits[i]]:
                 currStr.append(ch)
-                dfs(i + 1, currStr)
+                backtrack(i+1)
                 currStr.pop()
 
-        if digits == "":
-            return []
-
-        dfs(0, [])
+        backtrack(0)
         return res
