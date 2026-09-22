72d5e94426f04d6983044caa2257424dc73f2b04
Sahil Atul Mhatre <64244110+Sahil-Mhatre14@users.noreply.github.com>
2026-09-21 23:07:34 -0700

    Time: 4 ms (2.26%), Space: 19.6 MB (10.47%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,7 +1,6 @@
 class Solution:
-    def lastStoneWeight(self, stones: List[int]) -> int:
+    def lastStoneWeight(self, stones: list[int]) -> int:
         maxHeap = []
-
         for stone in stones:
             maxHeap.append(-stone)
 
@@ -11,7 +10,7 @@ class Solution:
             y = -1 * heapq.heappop(maxHeap)
             x = -1 * heapq.heappop(maxHeap)
 
-            if not x == y:
+            if x != y:
                 heapq.heappush(maxHeap, -1 * (y - x))
         
         return -1 * maxHeap[0] if len(maxHeap) > 0 else 0
